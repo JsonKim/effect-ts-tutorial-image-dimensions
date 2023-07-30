fd773b1cb6fa4b4fe02e853eef02f68b79a177ad
Jason Kim <actcrazy@gmail.com>
2023-07-30 12:32:40 +0900

    feat: 선택한 파일의 이미지 크기 보여주기

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,44 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+type ImageSize = { width: number; height: number };
+type FileStatus =
+  | { _tag: "NotSelected" }
+  | { _tag: "NotImage" }
+  | { _tag: "FileError" }
+  | { _tag: "Selected"; size: ImageSize };
+
 function App() {
   const [count, setCount] = useState(0);
+  const [status, setStatus] = useState<FileStatus>({ _tag: "NotSelected" });
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file === undefined) {
+      setStatus({ _tag: "NotSelected" });
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      const text = reader.result;
+      const img = new Image();
+      img.onload = () => {
+        setStatus({
+          _tag: "Selected",
+          size: { width: img.width, height: img.height },
+        });
+      };
+      img.onerror = () => {
+        setStatus({ _tag: "NotImage" });
+      };
+      img.src = text as string;
+    };
+    reader.onerror = () => {
+      setStatus({ _tag: "FileError" });
+    };
+    reader.readAsDataURL(file);
+  };
 
   return (
     <div className="App">
@@ -16,6 +52,16 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+      </div>
+      <div>
+        {status._tag === "NotSelected" && "파일이 선택되지 않았습니다."}
+        {status._tag === "NotImage" && "이미지 파일이 아닙니다."}
+        {status._tag === "FileError" && "파일을 읽는 중에 에러가 발생했습니다."}
+        {status._tag === "Selected" &&
+          `이미지 크기: ${status.size.width}x${status.size.height}`}
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
